367e686393fa91736357c105d18e414bccf93119
Stormtrooper7652 <gabrielvliegenthart@gmail.com>
2023-10-22 14:11:28 +0200

    fixed up some js, still not working

--- landing/scripts.js
+++ landing/scripts.js
@@ -1,37 +1,40 @@
 var dynamicLogo = document.getElementById(`heading`);
 var websiteName = "dynamic";
 var glitchyCharacters = ["$", "@", "&", "!", "%", "#", "?", "Δ", "Θ", "Λ", "Ξ", "Π", "Σ", "Φ", "Ψ", "Ω", "+", "/", "=", "<", ">", "≠", "≤", "≥", "∞", "∑", "∫", "∆"];
-var runFrame = setInterval(frame, 100); // Remove the parentheses here
+//var runFrame = setInterval(frame, 1000); // Remove the parentheses here
 
-// clearInterval(intervalId);
 var cutFrame = 0
 function frame() {
-    if (cutFrame <= 100) {
-        randomCharacter()
+    if (cutFrame < 20) {
+        changeCharacter()
         cutFrame++
     } else  {
         clearInterval(runFrame);
+        dynamicLogo.innerText = websiteName
     }
-    console.log(cutFrame)
 }
 
 function randomCharacter() {
-  var randomIndex = Math.floor(Math.random() * glitchyCharacters.length);
-  var character = glitchyCharacters[randomIndex];
-  changeCharacter(character);
+    var randomIndex = Math.floor(Math.random() * glitchyCharacters.length);
+    var character = glitchyCharacters[randomIndex];
+    return character
 }
 
-function changeCharacter(character) {
-  var newHeadingText = "";
-  for (var i = 0; i < websiteName.length; i++) {
-    var originalCharacter = websiteName.charAt(i);
-    if (glitchyCharacters.includes(originalCharacter)) {
-      newHeadingText += originalCharacter;
-    } else {
-      newHeadingText += character;
-    }
+function changeCharacter() {
+  var newHeadingText = [];
+  console.log(websiteName.length)
+  for (let i = 0; i < websiteName.length; i++) {
+    const ele = randomCharacter()
+    newHeadingText.push(ele)
+    console.log(ele)
   }
-  renderHeading(newHeadingText);
+  console.log(newHeadingText)
+  var finalText = "";
+  for (let i = 0; i < newHeadingText.length; i++) {
+    const element = newHeadingText[i];
+    finalText = finalText + element
+  }
+  renderHeading(finalText);
 }
 
 function renderHeading(headingText) {
