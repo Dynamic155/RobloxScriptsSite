0091aee3b582a2611c1ef848f046feac2fa04901
Stormtrooper7652 <gabrielvliegenthart@gmail.com>
2023-10-23 14:45:28 +0200

    changed heading postion to relative fixing window

--- landing/scripts.js
+++ landing/scripts.js
@@ -1,8 +1,3 @@
-//var dynamicLogo = document.getElementById(`webName`);
-//document.getElementById(`startOfheading`).innerText = "</"
-//document.getElementById(`endOfheading`).innerText = ">"
-//var websiteName = "dynamic";
-//var runFrame = setInterval(frame, 100); // Remove the parentheses here
 var line = document.querySelector(`#liner`)
 const luaUtext = document.querySelector(`#lua`)
 const letters = `ABCDEFGHIJKLMNOPQRWSTUVWXYZ%%#@&$∞$@&!%#?”˜›£¦¨©+/=‰‰¤‰¥‘«`;
@@ -33,59 +28,73 @@ function fadeAudioPause() {
 badCPUaudio.play()
 
 window.addEventListener("load", () => {
-    const heading = document.querySelector("#heading");
-    if (heading) {
-      let iteration = -2;
-      let originalText = heading.innerText;
-      clearInterval(interval);
-  
-        interval = setInterval(() => {
-        heading.innerText = originalText
-          .split("")
-          .map((letter, index) => {
-            if (index < iteration) {
-              return originalText[index];
-            }
-            return letters[Math.floor(Math.random() * letters.length)];
-          })
-          .join("");
-          fadeAudioPause()
-  
-        if (iteration >= originalText.length) {
-          clearInterval(interval);
-          setTimeout(() => {
-            var frame = 0
-            var width = 0
-            runFrame = setInterval(() => {
+	const heading = document.querySelector("#heading");
+	if (heading) {
+		let iteration = -2;
+		let originalText = heading.innerText;
+		clearInterval(interval);
 
-                frame++
-                width = width + 6
-                line.innerHTML = `<hr id="line" style="translate: ${-304 + width/2}px -39px; width: ${width}px;">`
-                if (frame > 400/4){
-                    clearInterval(runFrame)
+    //text animation
+		interval = setInterval(() => {
+			heading.innerText = originalText
+				.split("")
+				.map((letter, index) => {
+					if (index < iteration) {
+						return originalText[index];
+					}
+					return letters[Math.floor(Math.random() * letters.length)];
+				})
+				.join("");
+			fadeAudioPause()
 
+			if (iteration >= originalText.length) {
+				clearInterval(interval);
+        //line animation
+				setTimeout(() => {
+					var frame = 0
+					var width = 0
+					lineAnimation = setInterval(() => {
+
+						frame++
+						width = width + 6
+						line.innerHTML = `<hr id="line" style="translate: ${-304 + width/2}px -39px; width: ${width}px;">`
+						if (frame > 400 / 4) {
+							clearInterval(lineAnimation)
+              //luaU animation
+							setTimeout(() => {
+								var frame = 0;
+								var height = 0;
+								var opacity = 0;
+								luaUAnimation = setInterval(() => {
+									frame++
+									opacity = opacity + 2
+									height = height + 0.13
+									luaUtext.innerHTML = `<h3 id="luaU" style="opacity: ${opacity - 170}%; translate: 50px ${-84 + height}px;">LuaU Scripting</h3>`
+									welcomeMessage.play()
+									if (frame > 150) {
+										clearInterval(luaUAnimation)
+                    //move text up animation
                     setTimeout(() => {
-                        var height = 0
-                        var frame = 0
-                        var opacity = 0
-                        otherAnimation = setInterval(() => {
-                            frame++
-                            opacity = opacity + 2
-                            height = height + 0.13
-                            luaUtext.innerHTML = `<h3 id="luaU" style="opacity: ${opacity - 170}%; translate: 50px ${-84 + height}px;">LuaU Scripting</h3>`
-                            welcomeMessage.play()
-                            if (frame > 150){
-                                clearInterval(otherAnimation)
-                            }
-                        }, 1);
-                      }, 100);
-                }
-            }, 2.5);
-          }, 250);
-        }
-  
-        iteration += 1/8; // Use 0.25 to increment by 1/4
-      }, 16);
-      
-    }
-  });
+                      var frame = 0;
+                      var hight = 0;
+                      var inputOpacity = 0;
+                      moveTextUp = setInterval(() => {
+                      
+                        if (frame > 150) {
+                          clearInterval(moveTextUp)
+                        }
+                      }, 5);
+                    }, 500);
+									}
+								}, 1);
+							}, 100);
+						}
+					}, 5);
+				}, 250);
+			}
+
+			iteration += 1 / 8; // Use 0.25 to increment by 1/4
+		}, 16);
+
+	}
+});
